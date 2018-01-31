b670e667fd718f30127d5ac9c6fda6e8a9e05050
tuomas.granlund <tuomas.granlund@mylab.fi>
2018-01-31 14:34:17 +0200

    Some basic structures of application added.

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Testiresepti',
+      'testikuvaus',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBlIAeL3h0ZXldD7Zqsu5qVv5aPsaVjY5Hd4cPUcaF3h5ZvA4A6Q')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
